4d6c80c26e0205b8ce54aa8bd3eeec6958f3147d
sas03 <stephanesumo@yahoo.fr>
2023-07-05 21:48:02 +0200

    Razor MVC sous C# .NET

---
+++ Controllers/ExpenditureController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorPrix.Models;
+
+namespace RazorPrix.Controllers
+{
+    public class ExpenditureController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Expenditure> expenditures = new List<Expenditure>
+            {
+                new Expenditure("Cafe", new DateTime(2023,4,17), 5),
+                new Expenditure("Sucre", new DateTime(2023,5,21), 2),
+                new Expenditure("Riz", new DateTime(2023,1,6), 7)
+            };
+            double Count = 0;
+
+            foreach(Expenditure exp in expenditures)
+            {
+                Count = Count + exp.Prix;
+            }
+            ViewData["Count"] = Count;
+
+            return View(expenditures);
+        }
+    }
+}
---
+++ Models/Expenditure.cs
@@ -0,0 +1,15 @@
+﻿namespace RazorPrix.Models
+{
+    public class Expenditure
+    {
+        public string Nom { get; set; }
+        public DateTime Date { get; set; }
+        public double Prix { get; set; }
+        public Expenditure(string nom, DateTime date, double prix)
+        {
+            Nom = nom;
+            Date = date;
+            Prix = prix;
+        }
+    }
+}
